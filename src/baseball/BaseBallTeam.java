f35630b3126ade1c50b7a3b9efdb9beb219f4a79
hh1993 <h.hiroyoshi1227@gmail.com>
2023-02-06 16:26:14 +0900

    課題提出

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,25 @@
+package baseball;
+
+public class BaseBallTeam {
+    //フィールド
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+    //コンストラクタ
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+       this.name = name;
+       this.win = win;
+       this.lose =lose;
+       this.draw= draw;
+
+    }
+    public void report() {
+
+        System.out.println(this.name + " の2022年の成績は"+ this.win + " 勝 " + this.lose + " 敗 " + this.draw +" 分、勝率は "+ getRate() + "です");
+    }
+    public double getRate() {
+        return (double)this.win/((double)this.win + (double)this.lose);
+    }
+
+}
